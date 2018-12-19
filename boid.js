aed1fb8f52658fdff179c2b03b2d880443434be6
David Bryant <david@ip0af5216a.int.uvm.edu>
2018-12-19 00:11:31 -0500

    lights

--- boid.js
+++ boid.js
@@ -1,59 +1,80 @@
-const BOID_RADIUS = 20;
+const BOID_RADIUS = 40;
+const SEPARATION_RADIUS = 50;
+const ALIGNMENT_RADIUS = 60;
+const COHESION_RADIUS = 20;
+
+const SEPARATION_FORCE = 0.5;
+const ALIGNMENT_FORCE = 0.4;
+const COHESION_FORCE = 1;
 
 class Boid {
   constructor(x, y, direction, initialSpeed) {
     this.position = createVector(x, y);
     this.heading = createVector(cos(direction), sin(direction));
     this.speed = initialSpeed;
+    this.separationRadius = SEPARATION_RADIUS;
+    this.alignmentRadius = ALIGNMENT_RADIUS;
+    this.cohesionRadius = COHESION_RADIUS;
   }
 
   step() {
-    // look at all other boids in radius
-    const neighbors = boids.filter(
-      boid => this.position.dist(boid.position) < BOID_RADIUS && this !== boid
-    );
-
-    // if (neighbors.includes(this)) console.log("includes");
-
     // separation: steer to avoid crowding local flockmates
-    this.avoidVector = neighbors
+    this.separationNeighbors = boids.filter(
+      boid =>
+        this.position.dist(boid.position) < SEPARATION_RADIUS && this !== boid
+    );
+    this.avoidVector = this.separationNeighbors
       .map(boid =>
         createVector(
           this.position.x - boid.position.x,
           this.position.y - boid.position.y
         )
-      ) // get position difference (normalized vec pointing away from neighbor)
+      ) // get position difference (vec pointing away from neighbor)
       .reduce((a, b) => a.add(b), createVector()); // sum vecs
-    const avoidVectorNorm = this.avoidVector.copy().normalize();
-    this.heading.add(avoidVectorNorm);
+    this.avoidVectorNorm = this.avoidVector.copy().normalize();
+    this.separationVector = this.avoidVectorNorm.copy().mult(SEPARATION_FORCE);
+    this.heading.add(this.separationVector);
 
     // alignment: steer towards the average heading of local flockmates
-    this.avgHeading = neighbors
+    this.alignmentNeighbors = boids.filter(
+      boid =>
+        this.position.dist(boid.position) < ALIGNMENT_RADIUS && this !== boid
+    );
+    this.avgHeading = this.alignmentNeighbors
       .map(n => n.heading) // get headings
       .reduce((acc, heading) => acc.add(heading), createVector()) // sum headings
-      .div(neighbors.length || 1); // divide by total neighbors (avg neighbor heading)
-    const avgHeadingNorm = this.avgHeading.copy().normalize(); // normalize (this step is hopefully redundant)
-    this.heading.add(avgHeadingNorm); // add to own heading
+      .div(this.alignmentNeighbors.length || 1); // divide by total this.neighbors (avg neighbor heading)
+    this.avgHeadingNorm = this.avgHeading.copy().normalize(); // normalize (this step is hopefully redundant)
+    this.alignmentVector = this.avgHeadingNorm.copy().mult(ALIGNMENT_FORCE); // normalize (this step is hopefully redundant)
+    this.heading.add(this.alignmentVector);
 
     // cohesion: steer to move toward the average position (center of mass) of local flockmates
-    this.avgPosition = neighbors
+    this.cohesionNeighbors = boids.filter(
+      boid =>
+        this.position.dist(boid.position) < COHESION_RADIUS && this !== boid
+    );
+    this.avgPosition = this.cohesionNeighbors
       .map(n => n.position) // get positions
       .reduce((acc, position) => acc.add(position), createVector()) // sum positions
-      .div(neighbors.length || 1); // divide by total neighbors (avg neightbor heading)
-    const avgPositionNorm = this.avgPosition.copy().normalize(); // normalize
-    this.heading.add(avgPositionNorm); // add to own heading
+      .div(this.cohesionNeighbors.length || 1); // divide by total this.cohesionNeighbors (avg neightbor heading)
+    this.avgPositionNorm = this.avgPosition.copy().normalize(); // normalize
+    this.cohesionVector = this.avgPositionNorm.copy().mult(COHESION_FORCE); // normalize
+    this.heading.add(this.cohesionVector);
 
     // normalize heading
-    this.heading.normalize();
+    // this.heading.normalize();
+
+    this.heading.limit(4);
 
     // move
+    // const displacement = this.heading;
     const displacement = p5.Vector.mult(this.heading, this.speed);
     this.position.add(displacement);
 
     // wrap
-    if (this.position.x > width) this.position.x = 0;
-    if (this.position.x < 0) this.position.x = width;
-    if (this.position.y > height) this.position.y = 0;
-    if (this.position.y < 0) this.position.y = height;
+    if (this.position.x > width + BOID_RADIUS) this.position.x = -BOID_RADIUS;
+    if (this.position.x < -BOID_RADIUS) this.position.x = width + BOID_RADIUS;
+    if (this.position.y > height + BOID_RADIUS) this.position.y = -BOID_RADIUS;
+    if (this.position.y < -BOID_RADIUS) this.position.y = height + BOID_RADIUS;
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,26 +1,12 @@
-const BOIDS = 200;
+const BOIDS = 100;
 
 const boids = [];
 
 let paused = false;
 
-function setup() {
-  createCanvas(600, 400);
-
-  for (let i = 0; i < BOIDS; i++) {
-    boids.push(new Boid(random(width), random(height), random(PI * 2), 1));
-  }
-
-  for (boid of boids) {
-    boid.step();
-  }
-
-  // frameRate(10);
-}
-
 function drawBoid(boid) {
   stroke(255);
-  strokeWeight(2);
+  strokeWeight(8);
   point(boid.position.x, boid.position.y);
 }
 
@@ -32,12 +18,12 @@ function drawBoidFov(boid) {
 }
 
 function drawBoidVector(boid) {
-  stroke(255, 127);
+  stroke(255);
   strokeWeight(1);
   noFill();
   const boidPlusSpeed = p5.Vector.add(
     boid.position,
-    p5.Vector.mult(boid.heading, boid.speed).mult(10)
+    p5.Vector.mult(boid.heading, boid.speed).mult(12)
   );
   line(boid.position.x, boid.position.y, boidPlusSpeed.x, boidPlusSpeed.y);
 }
@@ -54,6 +40,52 @@ function drawAvgPos(boid) {
     );
 }
 
+function drawAvoidVector(boid) {
+  stroke(255, 0, 0, 200);
+  strokeWeight(1);
+  if (boid.avgPosition.x && boid.avgPosition.y)
+    line(
+      boid.position.x,
+      boid.position.y,
+      p5.Vector.add(boid.position, boid.avoidVectorNorm).x,
+      p5.Vector.add(boid.position, boid.avoidVectorNorm).y
+    );
+}
+
+function drawHalo(boid, radius, color, weight, fillColor) {
+  stroke(color);
+  strokeWeight(weight);
+  fill(fillColor);
+  ellipse(boid.position.x, boid.position.y, radius * 2, radius * 2);
+}
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+
+  for (let i = 0; i < BOIDS; i++) {
+    boids.push(
+      new Boid(
+        width / 4 + random(width - width / 2),
+        height / 4 + random(height - height / 2),
+        random(PI * 2),
+        2
+      )
+    );
+  }
+
+  for (boid of boids) {
+    boid.step();
+  }
+
+  // const c = color("rgba(255,0,0,200)");
+  // console.log(c);
+  // stroke(c);
+  // stroke(color('#ff0000'))
+  // ellipse()
+
+  // frameRate(10);
+}
+
 function draw() {
   if (paused) return;
 
@@ -61,10 +93,47 @@ function draw() {
 
   // draw boids
   for (boid of boids) {
-    drawBoid(boid);
     // drawBoidFov(boid);
+
+    drawHalo(
+      boid,
+      boid.cohesionRadius,
+      color("#ff000080"),
+      1,
+      color("#ff000080")
+    );
+
+    drawHalo(
+      boid,
+      boid.separationRadius,
+      color("#ff006640"),
+      1,
+      color("#ff006640")
+    );
+
+    drawHalo(
+      boid,
+      boid.alignmentRadius,
+      color("#ff00ff20"),
+      1,
+      color("#ff00ff20")
+    );
+
     // drawBoidVector(boid);
+    // drawAvoidVector(boid);
     // drawAvgPos(boid);
+
+    strokeWeight(1);
+    stroke(255);
+    for (n of boid.alignmentNeighbors) {
+      if (
+        dist(boid.position.x, boid.position.y, n.position.x, n.position.y) <
+        max(max(SEPARATION_RADIUS, ALIGNMENT_RADIUS), SEPARATION_RADIUS)
+      )
+        line(boid.position.x, boid.position.y, n.position.x, n.position.y);
+    }
+
+    drawBoid(boid);
   }
 
   for (boid of boids) {
